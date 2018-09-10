d32b30d744570b3a144f7495cda13ca815ee2746
Lin, Cheng-Tao <mutolisp@gmail.com>
2018-09-10 23:06:53 +0800

    revise the display of translation term context

--- generate_crowdin_files.sh
+++ generate_crowdin_files.sh
@@ -3,8 +3,8 @@
 classes=(Event Record-levels Occurrence Taxon MaterialSample Organism Identification Location GeologicalContext MeasurementOrFacts ResourceRelationship)
 for (( i=0;i<=10; i++))
 do
-	awk -F'\t' 'NR>1 {print $1":Term\t"$4"\t"$5"\t"$6}' vocabularies/${classes[${i}]}.tsv | grep -v "\t\t\t" > dist/${classes[${i}]}_Terms.csv
-	awk -F'\t' 'NR>1 {print $1":Definition\t"$6"\t"$7"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
-	awk -F'\t' 'NR>1 {print $1":Comments\t"$8"\t"$9"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
-	awk -F'\t' 'NR>1 {print $1":Examples\t"$10"\t"$11"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
+    awk -F'\t' 'NR>1 {print $1":Term ("$12")\t"$4"\t"$5"\t"$6}' vocabularies/${classes[${i}]}.tsv | grep -v "\t\t\t" > dist/${classes[${i}]}_Terms.csv
+    awk -F'\t' 'NR>1 {print $1":Definition of "$4" ("$12")\t"$6"\t"$7"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
+    awk -F'\t' 'NR>1 {print $1":Comments of "$4" ("$12")\t"$8"\t"$9"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
+    awk -F'\t' 'NR>1 {print $1":Examples of "$4" ("$12")\t"$10"\t"$11"\t"}' vocabularies/${classes[${i}]}.tsv |  grep -v "\t\t\t" >> dist/${classes[${i}]}_Terms.csv
 done
